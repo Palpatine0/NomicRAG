670e6f50148ef86c13e030f2567216fa9b564f44
Percival <emperorsheevpalpatine0@gmail.com>
2024-06-13 19:56:33 +0800

    feat: Add web content retrieval and vector store integration with Chroma and NomicEmbeddings
    - Added web content retrieval and document splitting using `WebBaseLoader` and `CharacterTextSplitter`.
    - Integrated `Chroma` vector store with `NomicEmbeddings` for storing and retrieving document embeddings.
    - Implemented a retriever to get relevant documents based on queries.

---
+++ nomic-rag/app/chain.py
@@ -0,0 +1,41 @@
+from langchain_community.chat_models import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
+from langchain_nomic import NomicEmbeddings
+from langchain_nomic.embeddings import NomicEmbeddings
+
+from dotenv import load_dotenv
+from langchain.callbacks.tracers.langchain import wait_for_all_tracers
+
+load_dotenv()
+wait_for_all_tracers()
+
+# Web content retrieval, aggregation, and character-based document splitting.
+urls = [
+    "https://lilianweng.github.io/posts/2023-06-23-agent/",
+    "https://lilianweng.github.io/posts/2023-03-15-prompt-engineering/",
+    "https://lilianweng.github.io/posts/2023-10-25-adv-attack-llm/",
+]
+
+docs = [WebBaseLoader(url).load() for url in urls]
+docs_list = [item for sublist in docs for item in sublist]
+
+text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size = 7500, chunk_overlap = 100
+)
+doc_splits = text_splitter.split_documents(docs_list)
+
+# Add to vectorDB
+vectorstore = Chroma.from_documents(
+    documents = doc_splits,
+    collection_name = "rag-chroma",
+    embedding = NomicEmbeddings(model = "nomic-embed-text-v1"),
+)
+retriever = vectorstore.as_retriever()
+
+print(retriever.get_relevant_documents("Talk decomposition"))
